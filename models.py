90b8616f56085bba91906bd0eac2e928593e5a56
Ray Nelson <rayolinels@gmail.com>
2021-04-15 11:03:12 -0700

    Switched scaffold out with scaffold_bulma from the professor
    from https://bitbucket.org/luca_de_alfaro/scaffold_bulma/src/master/

--- models.py
+++ models.py
@@ -2,14 +2,22 @@
 This file defines the database models
 """
 
-from .common import db, Field
+import datetime
+from .common import db, Field, auth
 from pydal.validators import *
+
+
+def get_user_email():
+    return auth.current_user.get('email') if auth.current_user else None
+
+def get_time():
+    return datetime.datetime.utcnow()
+
 
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
-#
-# db.commit()
-#
+
+db.commit()
